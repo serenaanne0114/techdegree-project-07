6308b670edf1eccbde76f1f2c89c5287dd2410d6
serenaanne0114 <56902316+serenaanne0114@users.noreply.github.com>
2020-09-16 20:06:41 -0700

    update

--- techdegree-project-07/web_app_dashboard_v3.1/js/data.js
+++ techdegree-project-07/web_app_dashboard_v3.1/js/data.js
@@ -1,6 +1,9 @@
 //Chart js
-var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
+const traffic = document.getElementById('myChart').getContext('2d');
+const daily = document.getElementById('dailyChart').getContext('2d');
+const mobile = document.getElementById('doughnutChart').getContext('2d');
+
+var chart1 = new Chart(traffic, {
     // The type of chart we want to create
     type: 'line',
 
@@ -17,7 +20,7 @@ var chart = new Chart(ctx, {
             pointBorderWidth:'2',
             pointBorderColor: 'rgba(116, 120, 191, 1)',
             lineTension: 0,
-            data: [500, 1000, 1500, 2000, 2500]
+            data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500]
         }]
     },
 
@@ -33,95 +36,65 @@ var chart = new Chart(ctx, {
     }    
 });
 
-// let trafficData1 = [350, 1450, 1000, 1200, 1500, 950, 580, 1050, 1250, 1000, 1500];
 
-// let trafficData2 = [950, 1250, 400, 1500, 1500, 2550, 100, 1350, 2250, 1200, 1500];
-
-// let trafficData3 = [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500];
-
-// let trafficData4 = [750, 1750, 1500, 1800, 1500, 2050, 840, 1350, 950, 700, 1500];
-
-// const trafficNav = document.querySelector('.traffic-nav');
-// const trafficAll = document.querySelector('.traffic-nav-link');
-
-// trafficNav.addEventListener('click', e => {
-//     const navSelected = e.target;
-
-//     if (navSelected.tagName === 'LI') {
-//         trafficAll.forEach(element =>{
-//             element.classList.remove('active');
-//         });
-//         navSelected.classList += 'active';
-//     }
-
-//     if (navSelected.innerText === 'Hourly') {
-//         trafficChart.data.datasets[0].data = trafficData1;
-//         trafficChart.update();
-//     }
-//      else if (navSelected.innerText === 'Daily') {
-//         trafficChart.data.datasets[0].data = trafficData2;
-//         trafficChart.update();
-//     }
-//      else if (navSelected.innerText === 'Weekly') {
-//         trafficChart.data.datasets[0].data = trafficData3;
-//         trafficChart.update();
-//     }
-//      else if (navSelected.innerText === 'Monthly') {
-//         trafficChart.data.datasets[0].data = trafficData4;
-//         trafficChart.update();
-//     }
-// });
 
 //Bar chart
-var myBarChart = new Chart(ctx, {
+var chart2 = new Chart(daily, {
+    // The type of chart we want to create
     type: 'bar',
-    labels: ['S', 'M', 'T', 'W', 'T', 'F','S'],
-    datasets: [{
-    data: [10, 20, 30, 40, 50, 60, 70],
-    backgroundColor: '#7477BF',
-    borderWidth: 1 }]
-});
 
-
-let dailyOptions = {
-    scales:{
-        yAxes: [{
-            ticks: {
-                beginAtZero: true
-            }
+    // The data for our dataset
+    data: {
+        labels: ['S', 'M', 'T', 'W', 'T', 'F','S'],
+        datasets: [{
+            label: '# of Hits',
+            barPercentage: 0.5, 
+             barThickness: 25,   
+             maxBarThickness: 30,
+            minBarLength: 2,
+            data: [10, 20, 30, 40, 50, 60, 70],
+             backgroundColor: '#7477BF',
+            borderWidth: 1
         }]
     },
-    legend : {
-        display: false
-    },
-    layout: {
-        padding: {
-            left: 10, 
-            right: 40,
-            bottom: 30
-        }
-    }
-}
 
-//mobile users
-var myDoughnutChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: data,
-    options: options
+    // Configuration options go here
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }    
 });
 
- let mobileData = {
-     legend: {
-         position: 'right',
-         },
-    datasets: [{
-        data: [10, 20, 30]
-    }],
 
-    // These labels appear in the legend and in the tooltips when hovering different arcs
-    labels: [
-        'Phones',
-        'Tablets',
-        'Desktop'
-    ]
-};
+//mobile users
+var chart3 = new Chart(mobile, {
+    // The type of chart we want to create
+    type: 'doughnut',
+
+    // The data for our dataset
+    data: {
+        labels: ["Phones", "Tablets", "Desktop"],
+        datasets: [{
+            label: '# of Users',
+            data: [550, 500, 2000],
+            borderWidth: 0,
+            backgroundColor: ["#78CF82", "#51B6C8","7477BF"],
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+        legend: {
+            position: "right",
+            label: {
+                boxWidth: 20,
+                fontStyle: "bold"
+            }
+    } 
+},   
+});
