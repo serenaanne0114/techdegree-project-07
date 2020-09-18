906387226757b2ad4d4ecc2c491c98a637925813
serenaanne0114 <56902316+serenaanne0114@users.noreply.github.com>
2020-09-17 19:00:44 -0700

    update

--- techdegree-project-07/web_app_dashboard_v3.1/js/app.js
+++ techdegree-project-07/web_app_dashboard_v3.1/js/app.js
@@ -1,20 +1,20 @@
 
 
 
-// //Alert Notification
-// const alertNotification = document.getElementById('alert');
+//Alert Notification
+const alertNotification = document.getElementById('alert');
 
-// alertNotification.innerHTML =
-// `<div class="alert-notification">
-//     <p><strong>ALERT!</strong>THERE ARE<strong>6</strong><strong>NEW NOTIFICATIONS</strong></p><p class="alert-notication-close"></p>
-// </div>`
+alertNotification.innerHTML =
+`<div class="alert-notification">
+    <p><strong>ALERT!</strong>THERE ARE<strong>6</strong><strong>NEW NOTIFICATIONS</strong></p><p class="alert-notication-close"></p>
+</div>`
 
-// alertNotification.addEventListener ('click',(e) => {
-//     const element = e.target;
-//     if(elemnt.classList.contains('alert-notification-close')) {
-//         alertNotification.style.display = 'none';
-//     }
-// });
+alertNotification.addEventListener ('click',(e) => {
+    const element = e.target;
+    if(elemnt.classList.contains('alert-notification-close')) {
+        alertNotification.style.display = 'none';
+    }
+});
 
 // //Traffic Chart
 // const trafficChart = document.getElementById('.traffic-chart');
@@ -133,22 +133,22 @@
 //     type: 'doughnut'
 // });
 
-// //User Search
-// const user = document.getElementById('fieldUser');
-// const message = document.getElementById("fieldMessage");
-// const send = document.getElementById('send');
+//User Search
+const user = document.getElementById('fieldUser');
+const message = document.getElementById("fieldMessage");
+const send = document.getElementById('send');
 
-// send.addEventListener('click', (e) => {
-//     if(user.value && message.value === ""){
-//         alert("Please fill out user message fields before sending");
-//     } else if (user.value === "") {
-//         alert("Please fill out message field before sending");
-//     } else if (message.value === "") {
-//         alert("Please fill out user field before sending");
-//     } else {
-//         alert("Message sent to $(user.value)");
-//     }
-// });
+send.addEventListener('click', (e) => {
+    if(user.value && message.value === ""){
+        alert("Please fill out user message fields before sending");
+    } else if (user.value === "") {
+        alert("Please fill out message field before sending");
+    } else if (message.value === "") {
+        alert("Please fill out user field before sending");
+    } else {
+        alert("Message sent to $(user.value)");
+    }
+});
 
 
 
