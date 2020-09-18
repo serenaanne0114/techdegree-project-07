478b5e5952dbbdf5dd05516a7ef8513f08d71084
serenaanne0114 <56902316+serenaanne0114@users.noreply.github.com>
2020-09-18 13:06:00 -0700

    update

--- techdegree-project-07/web_app_dashboard_v3.1/js/app.js
+++ techdegree-project-07/web_app_dashboard_v3.1/js/app.js
@@ -1,24 +1,24 @@
 //Alert Notification
-const alertBox = document.querySelectorAll('.alert');
-const notificationBell = document.querySelector('.bell-border');
-const mainHeader = document.querySelector('.main-header');
+// const alertBox = document.querySelectorAll('.alert');
+// const notificationBell = document.querySelector('.bell-border');
+// const mainHeader = document.querySelector('.main-header');
 
 
 
-// //Alert Notification
-// const alertNotification = document.getElementById('alert');
+//Alert Notification
+const alertNotification = document.querySelectorAll('.alert');
 
-// alertNotification.innerHTML =
-// `<div class="alert-notification">
-//     <p><strong>ALERT!</strong>THERE ARE<strong>6</strong><strong>NEW NOTIFICATIONS</strong></p><p class="alert-notication-close"></p>
-// </div>`
+alertNotification.innerHTML =
+`<div class="alert-notification">
+    <p><strong>ALERT!</strong>THERE ARE<strong>6</strong><strong>NEW NOTIFICATIONS</strong></p><p class="alert-notication-close"></p>
+</div>`
 
-// alertNotification.addEventListener ('click',(e) => {
-//     const element = e.target;
-//     if(elemnt.classList.contains('alert-notification-close')) {
-//         alertNotification.style.display = 'none';
-//     }
-// });
+alertNotification.addEventListener ('click',(e) => {
+    const element = e.target;
+    if(elemnt.classList.contains('alert-notification-close')) {
+        alertNotification.style.display = 'none';
+    }
+});
 
 // //User Search
 // const user = document.getElementById('fieldUser');
